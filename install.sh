fe07616d95b9956a0d9c88a1572b2a4bbadceac3
ArnaldWeger <arnaldweger@gmail.com>
2026-07-05 02:15:21 -0300

    Stack2 - Nova versão

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -e
 export USER_CONFIG_DIR="$HOME/.config"
 
 source ./scripts/00-pacotes.sh
-source ./scripts/config-i3.sh
-source ./scripts/config-i3status.sh
-source ./scripts/config-dunst.sh
+source ./.config/config-i3.sh
+source ./.config/config-i3status.sh
+source ./.config/config-dunst.sh
 
 echo "=== Instalação concluída! Reinicie a máquina. ==="
