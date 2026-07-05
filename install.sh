d707c6315e61a8de5ae055e68adc9a4e421fd68b
Arnald Didini  Weger <arnaldweger@gmail.com>
2026-07-05 01:42:38 -0300

    Clean up install.sh by removing commented scripts
    Removed unused script sources from installation process.

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ set -e
 export USER_CONFIG_DIR="$HOME/.config"
 
 source ./scripts/00-pacotes.sh
-source ./scripts/01-ly.sh
 source ./scripts/config-i3.sh
 source ./scripts/config-i3status.sh
 source ./scripts/config-dunst.sh
-source ./scripts/config-kitty.sh
 
 echo "=== Instalação concluída! Reinicie a máquina. ==="
