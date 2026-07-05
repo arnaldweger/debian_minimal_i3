5b342d86bd33ba34366fe59a830235261082a306
Arnald Didini  Weger <arnaldweger@gmail.com>
2026-07-05 01:42:13 -0300

    Refactor install.sh to source configuration scripts
    Removed installation steps for Debian Minimal i3 and added sourcing of configuration scripts.

--- install.sh
+++ install.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
-
 set -e
+export USER_CONFIG_DIR="$HOME/.config"
 
-echo "=== Iniciando a instalação do Debian Minimal i3 ==="
+source ./scripts/00-pacotes.sh
+source ./scripts/01-ly.sh
+source ./scripts/config-i3.sh
+source ./scripts/config-i3status.sh
+source ./scripts/config-dunst.sh
+source ./scripts/config-kitty.sh
 
-# 1. Atualizar e instalar pacotes essenciais
-sudo apt update
-sudo apt install -y \
-    i3-wm i3status j4-dmenu-desktop dmenu kitty dunst micro \
-    flameshot file-roller thunar git xinit xserver-xorg-core xserver-xorg \
-    build-essential libpam0g-dev
-
-# 2. Instalação do Ly Display Manager
-echo "=== Instalando o Ly Display Manager ==="
-cd /tmp
-if [ -d "ly" ]; then rm -rf ly; fi
-git clone --recurse-submodules https://github.com/fairyglade/ly.git
-cd ly
-make
-sudo make install
-sudo systemctl enable ly.service
-
-# Return to script directory
-cd - > /dev/null
-
-# 3. Copiar as configurações para o diretório do usuário
-echo "=== Aplicando configurações (Dotfiles) ==="
-mkdir -p "$HOME/.config"
-
-# Copia recursivamente respeitando os arquivos do repositório
-cp -r .config/* "$HOME/.config/"
-
-echo "=== Instalação concluída com sucesso! ==="
-echo "Por favor, reinicie a máquina."
+echo "=== Instalação concluída! Reinicie a máquina. ==="
